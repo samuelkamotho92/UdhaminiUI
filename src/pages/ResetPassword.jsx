46d66eed14945bd88d5fff35155cf28d76a8d95c
Denis Wachira <deniswachira77@gmail.com>
2023-01-09 17:07:10 +0300

    added media query and resest page

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from 'react';
 import { useForm } from "react-hook-form";
 // import { Context } from '../context/Context';
-import login from "../images/Login.png";
+import reset from "../images/reset.png";
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { Context } from '../context/Context';
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 
 
-function Login() {
+function ResetPassword() {
   const { register, formState: { errors }, handleSubmit } = useForm();
   const [error, setError] = useState(false);
   const { dispatch } = useContext(Context);
@@ -42,8 +42,8 @@ function Login() {
 
 
   return (
-    <div className='grid mt-60px bg-base-200 loginPage'>
-      {
+    <div className='grid mt-60px bg-base-200 h-fit resetPage'>
+      {/* {
         loginMutation.isLoading ? (
           <div className="alert alert-error mt-60px shadow-lg w-fit z-50 text-center text-white absolute top-0 right-0" >
             <div><span className='text-2xl'>😒</span>
@@ -60,46 +60,36 @@ function Login() {
           )
         )
 
-      }
+      } */}
       <div className="hero-content flex-col lg:flex-row-reverse justify-around ">
         <div className="hero-content ">
-          < div className="card flex-shrink-0 w-full max-w-sm shadow-2xl p-2 bg-base-200 place-self-center" >
-            <div className="card-header loginHeader">
-              <h1 className="text-5xl font-bold  px-2  text-center">Login  🔐</h1>
+          < div className="card flex-shrink-0  max-w-sm shadow-2xl p-2  place-self-center" >
+            <div className="card-header">
+              <h3 className="text-4xl font-bold  px-2 xs:text-3xl">Reset Password 🔐 </h3>
             </div>
             <div className="card-body">
               <form onSubmit={handleSubmit(onSubmit)} >
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text text-xl">Username</span>
+                    <span className="label-text text-xl">email</span>
                   </label>
-                  <input type="text" {...register("username", { required: true })} placeholder="Enter username" className="input input-warning input-lg input-bordered" />
-                  {errors.username?.type === 'required' && <p className="label-text-alt text-red-400 pt-2">username is required 😶</p>}
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text text-xl">Password</span>
-                  </label>
-                  <input type="password" {...register("password", { required: true })} placeholder="Enter password" className="input input-warning input-lg input-bordered" />
-                  {errors.password?.type === 'required' && <p className="label-text-alt text-red-400 pt-2">password is required😶</p>}
-                  <label className="label">
-                    <a href="#" className="label-text-alt link link-hover"><Link to="/reset">forgot password?</Link></a>
-                  </label>
-                </div>
+                  <input type="email" {...register("email", { required: true })} placeholder="Enter Email" className="input input-warning input-lg input-bordered" />
+                  {errors.email?.type === 'required' && <p className="label-text-alt text-red-400 pt-2">email is required 😶</p>}
+                </div>               
                 <div className="form-control mt-6">
-                  <button type="submit" className="btn btn-outline btn-warning">Login</button>
+                  <button type="submit" className="btn btn-outline btn-warning">Reset </button>
                 </div>
               </form>
             </div>
           </div >
         </div>
-        <div className="hero-content">
-          <img alt='no pic' src={login} className="loginImg xs:max-w-xs max-w-lg rounded-lg shadow-2xl" />
+        <div className="hero-content ">
+          <img alt='no pic' src={reset} className=" resetImg max-w-lg rounded-lg shadow-2xl" />
         </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default ResetPassword
 
