553663db3fc9df4a8f2ead4a78ce936b40c75345
Denis Wachira <deniswachira77@gmail.com>
2023-01-13 12:23:26 +0300

    admin

--- src/pages/AddScholarships.jsx
+++ src/pages/AddScholarships.jsx
@@ -1,48 +1,204 @@
-import React from 'react'
+import React from "react";
+import AdminProfileSidebar from "../components/AdminProfileSidebar";
+import { useState } from "react";
+import imagePlaceholder from "../images/placeholder.png";
+import { useForm } from "react-hook-form";
 import axios from "axios";
-import { useParams } from "react-router-dom";
-import scholar2 from '../images/scholar2.jpg'
-import UserProfileSidebar from '../components/UserProfileSidebar';
-import { useQuery } from "@tanstack/react-query";
-import { SpinningCircles } from 'react-loading-icons'
-import AdminProfileSidebar from '../components/AdminProfileSidebar';
-
 function AddScholarships() {
-    const { id } = useParams();
-    const { data, isLoading, error } = useQuery(["oneScholarships"], () =>
-        axios.post("https://udhamini-api.azurewebsites.net/api/scholarship/oneNonPremium", {
-            id: id
-        }), { retry: false });
-    const scholarshipData = data?.data[0];
-    return (
-        <div className='flex flex-row mt-60px md:h-full bg-base-200 '>
-            <AdminProfileSidebar />
-            <main className='container  ml-3 my-3 justify-center items-center h-full mt-6'>
-                {
-                    isLoading && <SpinningCircles stroke="#98ff98" strokeOpacity={.125} speed={.75} />
-                }
-                <div className="mockup-window border bg-base-300">
-                    <div className="flex justify-center px-4 py-16 bg-base-200">
-                        <div className="hero  bg-base-200">
-                            <div className="hero-content flex-col lg:flex-row">
-                                <img src={scholar2} className="max-w-lg rounded-lg shadow-2xl" />
-                                <div>
-                                    <h1 className="text-5xl uppercase font-bold">{scholarshipData?.name}</h1>
-                                    <div className="badge badge-secondary badge-lg my-2 text-xl">Scolarship Amount :{scholarshipData?.amount}</div>
-                                    <div className="py-2 md:text-lg lg:text-xl justify-center">
-                                        <p>👉Study-Level : {scholarshipData?.study_level}</p>
-                                        <p>👉Origin-Country : {scholarshipData?.origin_country}</p>
-                                        <p>👉Deadline : {scholarshipData?.deadline_day}</p>
-                                    </div>
-                                    <a href={scholarshipData?.link} className="btn btn-primary">Application Form Link</a>
-                                </div>
-                            </div>
-                        </div>
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [file, setFile] = useState(null);
+  const onSubmit = async (data) => {
+    try {
+      const newUser = {
+        fullname: data.fullname,
+        age: data.age,
+        gender: data.gender,
+        education_level: data.education_level,
+        gpa: data.gpa,
+        country: data.country,
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      };
+      if (file) {
+        const data = new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        newUser.profilepic = filename;
+        try {
+          await axios.post("/upload", data);
+        } catch (err) {}
+      }
+
+      const res = await axios.post("/auth/register", newUser);
+      setSuccess(true);
+      setTimeout(() => setError(false), 3000);
+      res.data && window.location.replace("/login");
+    } catch (error) {
+      setError(true);
+      setTimeout(() => setError(false), 4000);
+    }
+  };
+  return (
+    <div className="flex flex-row mt-60px md:h-full bg-base-200 ">
+      <AdminProfileSidebar />
+      <main className="container my-3 justify-center items-center h-full m-auto">
+        <main className="bg-base-200 mt-60px">
+          <div className="hero  bg-base-200">
+            <form>
+              <div className="hero-content flex-col ">
+                <div className="card ">
+                  <div className="card-header">
+                    <div className="avatar grid">
+                      <div className="w-24 rounded place-self-center">
+                        <label htmlFor="fileInput">
+                          {file ? (
+                            <img
+                              className="cursor-pointer"
+                              src={URL.createObjectURL(file)}
+                              alt="invalid Imagefile😒"
+                            />
+                          ) : (
+                            <img
+                              className="cursor-pointer"
+                              src={imagePlaceholder}
+                              alt="nopic"
+                            />
+                          )}
+                        </label>
+                        <input
+                          type="file"
+                          id="fileInput"
+                          style={{ display: "none" }}
+                          onChange={(e) => setFile(e.target.files[0])}
+                        />
+                      </div>
                     </div>
+                  </div>
+                  <div className="card-body md:flex-row sm:flex-col lg:flex-row">
+                    <div>
+                      <label className="label">
+                        <span className="label-text">Scholarship Name</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("fullname", { required: true })}
+                        placeholder="Scholarship"
+                        className="input input-bordered "
+                      />
+                      <label className="label">
+                        <span className="label-text">Study level</span>
+                      </label>
+                      <select
+                        {...register("education_level", { required: true })}
+                        className="select select-bordered w-full max-w-xs"
+                      >
+                        <option selected disabled value="">
+                          Select Study level
+                        </option>
+                        <option value="phd">PHD</option>
+                        <option value="postgraduate">Master</option>
+                        <option value="undergraduate">Degree</option>
+                      </select>
+                      {errors.education_level?.type === "required" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          study level is required 😶
+                        </p>
+                      )}
+
+                      <label className="label">
+                        <span className="label-text">Link</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("age", { required: true })}
+                        placeholder="Enter url"
+                        className="input input-bordered"
+                      />
+                      {errors.age?.type === "required" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          Link is required 😶
+                        </p>
+                      )}
+                    </div>
+                    <div className="md:ml-4 lg:ml-4">
+                      <label className="label">
+                        <span className="label-text">Deadline</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("gpa", { required: true })}
+                        placeholder="Deadline"
+                        className="input input-bordered"
+                      />
+                      {errors.gpa?.type === "required" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          Location is required 😶
+                        </p>
+                      )}
+                      <label className="label">
+                        <span className="label-text">Location</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("gpa", { required: true })}
+                        placeholder="Location"
+                        className="input input-bordered"
+                      />
+                      {errors.gpa?.type === "required" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          Location is required 😶
+                        </p>
+                      )}
+                      {errors.email?.type === "pattern" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          invalid email😶
+                        </p>
+                      )}
+                      <label className="label">
+                        <span className="label-text">Location</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("gpa", { required: true })}
+                        placeholder="Enter Location"
+                        className="input input-bordered"
+                      />
+                      {errors.gpa?.type === "required" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          Location is required 😶
+                        </p>
+                      )}
+                      {errors.email?.type === "pattern" && (
+                        <p className="label-text-alt text-red-400 pt-2">
+                          invalid email😶
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="form-control mt-3 grid">
+                    <button
+                      type="submit"
+                      className="btn btn-outline max-w-md place-self-center w-full"
+                    >
+                      Add Scholarship
+                    </button>
+                  </div>
                 </div>
-            </main>
-        </div>
-    )
+              </div>
+            </form>
+          </div>
+        </main>
+      </main>
+    </div>
+  );
 }
 
-export default AddScholarships
+export default AddScholarships;
--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -1,44 +1,58 @@
-import { useContext } from 'react';
-import UserProfileSidebar from '../components/UserProfileSidebar';
-import { Context } from '../context/Context';
-import { SpinningCircles } from 'react-loading-icons'
+import { useContext } from "react";
+import { Context } from "../context/Context";
+import { SpinningCircles } from "react-loading-icons";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import AdminProfileSidebar from "../components/AdminProfileSidebar";
 
 function Premium() {
-    const { user } = useContext(Context);
-    const authToken = user?.accessToken;
+  const { user } = useContext(Context);
+  const authToken = user?.accessToken;
 
-    const { data, isLoading, isError, error: myerror } = useQuery(["PremiumScholarships"], () =>
-        axios.post("https://udhamini-api.azurewebsites.net/api/scholarship/premium", {
-            premium_tier: user?.premium_tier_available
-        }, {
+  const {
+    data,
+    isLoading,
+    isError,
+    error: myerror,
+  } = useQuery(
+    ["PremiumScholarships"],
+    () =>
+      axios
+        .post(
+          "https://udhamini-api.azurewebsites.net/api/scholarship/premium",
+          {
+            premium_tier: user?.premium_tier_available,
+          },
+          {
             headers: {
-                'Content-Type': 'application/json',
-                "token": `Bearer ${user?.accessToken}`
-            }
-        },
-        ).then(response => response.data).catch(error => error)
-        , { retry: false });
-    console.log(data)
+              "Content-Type": "application/json",
+              token: `Bearer ${user?.accessToken}`,
+            },
+          }
+        )
+        .then((response) => response.data)
+        .catch((error) => error),
+    { retry: false }
+  );
+  console.log(data);
 
-
-    return (
-        <div className='flex flex-row mt-60px md:h-full bg-base-200 '>
-            <UserProfileSidebar />
-            <main className='container  grid md:grid-cols-3 sm:grid-cols-1 gap-5 justify-center items-center md:h-full sm:h-full mx-2 mb-3'>
-                <h2>scholarships</h2>
-                {
-                    isLoading ? (<SpinningCircles stroke="#98ff98" strokeOpacity={.125} speed={.75} />) :
-                        (isError && <div>{myerror}</div>
-
-                        )
-
-
-                }
-            </main>
-        </div>
-    )
+  return (
+    <div className="flex flex-row  md:h-full bg-base-200 ">
+      <AdminProfileSidebar />
+      <main className="container  grid md:grid-cols-3 sm:grid-cols-1 gap-5 justify-center items-center md:h-full sm:h-full mx-2 mb-3">
+        <h2>scholarships</h2>
+        {isLoading ? (
+          <SpinningCircles
+            stroke="#98ff98"
+            strokeOpacity={0.125}
+            speed={0.75}
+          />
+        ) : (
+          isError && <div>{myerror}</div>
+        )}
+      </main>
+    </div>
+  );
 }
 
-export default Premium
+export default Premium;
