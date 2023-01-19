afe987aa57d447871b5d7e203309f4dd6843e25f
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-01-19 21:38:40 +0300

    success and running

--- src/components/AdminProfileSidebar.jsx
+++ src/components/AdminProfileSidebar.jsx
@@ -4,17 +4,6 @@ import { Link } from "react-router-dom";
 import { Context } from "../context/Context";
 
 function AdminProfileSidebar() {
-<<<<<<< HEAD
-    const { user } = useContext(Context);
-    return (
-        <ul className="menu bg-base-300  items-start justify-start text-xl mt-20px h-full userprofile">
-            <li className='w-full'><Link to={`/adminprofile/`}><FaUser /> Profile</Link></li>
-            <li><Link to={`/admin/allscholarships`}><FaMoneyCheckAlt />All Scholarships</Link></li>     
-            <li><Link to={`/admin/addscholarships`}><FaMoneyCheckAlt /> Add Scholarships</Link></li>                     
-            <li><Link to={`/profile/premium`}><FaMoneyCheckAlt />Premium Tier</Link></li>            
-        </ul>
-    )
-=======
   const { user } = useContext(Context);
   return (
     <ul className="menu bg-base-300  items-start justify-start text-xl mt-20px h-full userprofile">
@@ -47,7 +36,6 @@ function AdminProfileSidebar() {
       </li>
     </ul>
   );
->>>>>>> 825f3965ffc9228d03913df259049a246c8d4230
 }
 
 export default AdminProfileSidebar;
